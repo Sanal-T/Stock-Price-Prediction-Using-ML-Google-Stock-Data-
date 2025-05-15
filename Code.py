131fb61b2078b6a7ccc50559758dad7fde3b55d1
Sanal-T <sanalt2024@gmail.com>
2025-05-15 12:56:31 +0530

    Updated code

--- Code.py
+++ Code.py
@@ -11,3 +11,10 @@ df.info()
 df.shape
 df=df.drop(columns=['symbol','adjClose','adjHigh','adjLow','adjOpen','adjOpen','divCash','splitFactor'],axis=1)
 df.head()
+
+df.describe()
+
+import plotly.graph_objects as go
+figure=go.Figure(data=[go.Candlestick(x=df['date'],open=df['open'],high=df['high'],low=df['low'],close=df['close'])])
+figure.update_layout(title='GOOG',xaxis_rangeslider_visible=False)
+figure.show()
